66f64b1c0242a43c36c1627ff093c426450db5a3
LauraMenzel <menzel-laura@gmx.de>
2022-11-12 14:30:23 +0100

    add header css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 const Home = () => {
   return <h1>Home</h1>;
 };
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,24 +1,35 @@
 import { Outlet, Link } from "react-router-dom";
-
+import "../App.css";
 
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/Recipe">Recipe</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+      <header>
+        <nav className="Layout-nav">
+          <ul className="Layout-ul">
+            <li className="Layout-li">
+              <Link className="Layout-Link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="Layout-li">
+              <Link className="Layout-Link" to="/Recipe">
+                Recipe
+              </Link>
+            </li>
+            <li className="Layout-li">
+              <Link className="Layout-Link" to="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+          <div className="Layout-searchBar">
+            <input type="text" placeholder="Search.." />
+          </div>
+        </nav>
 
-      <Outlet />
+        <Outlet />
+      </header>
     </>
   );
 };
