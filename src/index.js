77b85b1800af3214a440598317601f597a88145c
LauraMenzel <menzel-laura@gmx.de>
2022-11-12 13:30:38 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Recipe from "./pages/Recipe";
+import Layout from "./pages/Layout";
+import NoPage from "./pages/NoPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="Recipe" element={<Recipe />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
---
+++ src/pages/Recipe.js
@@ -0,0 +1,5 @@
+const Recipe = () => {
+  return <h1>Recipe Articles</h1>;
+};
+
+export default Recipe;
