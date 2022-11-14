67efd4c1553a9bb01be008a9467d13514f473596
Kairos22 <alandiko84@gmail.com>
2022-11-14 11:59:49 +0100

    add home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,21 @@
+import { useEffect } from "react";
 
+function App() {
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/categories.php"
+      );
 
-
-
-
-
+      const data = await response.json();
+      console.log(data);
+    };
+    getData();
+  }, []);
+}
 
 const Home = () => {
   return <h1>Home</h1>;
 };
 
-export default Home;
+export default App;
